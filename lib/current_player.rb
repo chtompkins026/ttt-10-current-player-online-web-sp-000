0442249e0faecaeae2b650d29f245f425976616d
Christopher Tompkins <chtompkins026@gmail.com>
2018-10-16 18:12:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,5 @@
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    current_player = "X"
-  else
-    current_player = "O"
-end
-return current_player
+ turn_count(board) % 2 == 0 ? current_player = "X" : current_player = "O"
+ return current_player 
 end
